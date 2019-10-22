08da67480a2495d399fc01cb9997b004aed2d8ca
pablowbk <pablo.weisbek@gmail.com>
2019-10-22 16:16:57 -0300

    basic animation for searchbox

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -12,39 +12,44 @@ class SearchBox extends Component {
     }
 
     this.expandSearchBox = this.expandSearchBox.bind(this);
+    this.searchInput = React.createRef()
   }
 
   expandSearchBox(event) {
     event.preventDefault();
     this.setState({
       inputExpanded: { width: "100%", paddingLeft: "20px", border: "2px solid" },
-      // btnExpanded: { right: "7px" },
-      isExpanded: true,
-    })
-    
-    console.log("expanding....");
+      btnExpanded: { right: "7px" },
+      isExpanded: true
+    });
+    this.searchInput.current.focus();
+
+    console.log("expanding... focusing...");
   }
 
   render() {
     const { handleSearchSubmit, handleInputChange } = this.props;
-    const { inputExpanded, isExpanded } = this.state;
+    const { inputExpanded, btnExpanded, isExpanded, inputFocus } = this.state;
     return (
       <div className="SearchBox">
         <span className="search-title">Search by City Name</span>
         <form 
           className="search-form" 
           onSubmit={isExpanded ? handleSearchSubmit : null}>
+          <input
+            className="search-input"
+            type="search"
+            placeholder="start typing..."
+            onChange={handleInputChange}
+            style={inputExpanded}
+            ref={this.searchInput} />
           <button 
             className={isExpanded ? `search-submit moveRight` : `search-submit`}
+            style={isExpanded ? btnExpanded : null }
             onClick={isExpanded ? handleSearchSubmit : this.expandSearchBox}>
             <img className="search-icon" src={icon} alt="search icon"/>
           </button>
-          <input 
-            className="search-input" 
-            type="search" 
-            placeholder="start typing..." 
-            onChange={handleInputChange}
-            style={inputExpanded} />
+          
         </form>
       </div>
     )
